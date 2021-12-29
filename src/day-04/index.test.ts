fe872e99e309642e9f8db937f2866a47e39b9aa8
Willy Brauner <mail@willybrauner.com>
2021-12-29 12:39:07 +0100

    refacto parseOneGrid WIP

--- src/day-04/index.test.ts
+++ src/day-04/index.test.ts
@@ -1,20 +1,73 @@
-import { parseOnGrid } from "."
+import { markValuesOfOneGrid, gridHasWinnerRow, convertColumnsToRows, parseOneGrid } from "."
 import formatInputs from "./formatInputs"
 
 const [PLAY_NUMBERS, GRIDS] = formatInputs("inputs-example.txt")
 
-it("should works", () => {
-//   console.log(PLAY_NUMBERS, GRIDS)
-  console.log(
-    parseOnGrid(
-      [
-        ["22", "13", "17", "11", "0"],
+// it("markValuesOfOneGrid should return marked values", () => {
+//   expect(
+//     markValuesOfOneGrid(
+//       [
+//         ["22", "13", "17", "11", "0"],
+//         ["8", "2", "23", "4", "24"],
+//         ["21", "9", "14", "16", "7"],
+//         ["6", "10", "3", "18", "5"],
+//         ["1", "12", "20", "15", "19"],
+//       ],
+//       ["1", "22", "10", "54", "11"]
+//     )
+//   ).toEqual([
+//     ["22**", "13", "17", "11**", "0"],
+//     ["8", "2", "23", "4", "24"],
+//     ["21", "9", "14", "16", "7"],
+//     ["6", "10**", "3", "18", "5"],
+//     ["1**", "12", "20", "15", "19"],
+//   ])
+// })
+
+it("gridHasWinnerRow should return true if all value are ended with **", () => {
+  expect(
+    gridHasWinnerRow([
+      ["22**", "13**", "17**", "11**", "0**"],
+      ["8", "2", "23", "4", "24"],
+      ["21**", "9**", "14**", "16", "7"],
+      ["6", "10", "3", "18", "5"],
+      ["1", "12", "20", "15", "19"],
+    ])
+  ).toEqual(true)
+})
+
+
+const array = [
+    ["22**", "13**", "17**", "11**", "0"],
+    ["8", "2", "23", "4", "24"],
+    ["21**", "9**", "14**", "16", "7"],
+    ["6", "10", "3", "18", "5"],
+    ["1", "12", "20", "15", "19"],
+  ]
+
+it("should convertColumnsToRows", () => {
+  
+  expect(convertColumnsToRows(array)).toEqual([
+    ["22**", "8", "21**", "6", "1"],
+    ["13**", "2", "9**", "10", "12"],
+    ["17**", "23", "14**", "3", "20"],
+    ["11**", "4", "16", "18", "15"],
+    ["0", "24", "7", "5", "19"],
+  ])
+})
+
+it("should parseOneGrid, ", () => {
+
+    const array = [
+        ["22", "13**", "17**", "1", "0"],
         ["8", "2", "23", "4", "24"],
-        ["21", "9", "14", "16", "7"],
+        ["21", "9**", "14**", "16", "7"],
         ["6", "10", "3", "18", "5"],
         ["1", "12", "20", "15", "19"],
-      ],
-      ["1", "22", "10", "54", "11"]
+      ]
+
+    console.log(
+        parseOneGrid(array, ["1", "22", "0", "54", "11"])
     )
-  )
+
 })
--- src/day-04/index.ts
+++ src/day-04/index.ts
@@ -1,3 +1,4 @@
+import { json } from "stream/consumers"
 import formatInputs, { TGrid, TPlayValues, TRow } from "./formatInputs"
 
 const [PLAY_NUMBERS, GRIDS] = formatInputs("inputs.txt")
@@ -12,27 +13,67 @@ const [PLAY_NUMBERS, GRIDS] = formatInputs("inputs.txt")
 
 // -------------------------------------------------------------------------- LOCAL
 
-/**
- * Parse each values and check if is matching with one playValue.
- * If match, return "value*" marked with '*' at the end
- * @param grid
- * @param playValues
- * @returns
- */
-export const markValuesOfOneGrid = (grid: TGrid, playValues: TPlayValues): TGrid => {
-  return grid.reduce<TGrid>((accGrid: TGrid, currRow: TRow): TGrid => {
-    // marked each value of of current row
-    const markedRow = currRow.reduce<TRow>((accValue: TRow, currValue: string): TRow => {
-      // check if current value is played
-      const valueIsPlayed = playValues.some((el) => el === currValue)
-      // prepare markvalue
-      const markValue = (value: string) => (value.endsWith("**") ? value : `${value}**`)
+// /**
+//  * Parse each values and check if is matching with one playValue.
+//  * If match, return "value*" marked with '*' at the end
+//  * @param grid
+//  * @param playValues
+//  */
+// export type TMarkValues = { grid: TGrid; lastCalledValue: number }
+// export const markValuesOfOneGrid = (
+//   grid: TGrid,
+//   playValues: TPlayValues
+// ): TMarkValues => {
+//   let justCalledValue = null
 
-      return [...accValue, valueIsPlayed ? markValue(currValue) : currValue]
-    }, [])
-    return [...accGrid, markedRow]
-  }, [])
+//   const newGrid = grid.reduce<TGrid>((accGrid: TGrid, currRow: TRow): TGrid => {
+//     // marked each value of of current row
+//     const markedRow = currRow.reduce<TRow>((accValue: TRow, currValue: string): TRow => {
+//       // check if current value is played
+//       const valueIsPlayed = playValues.some((el) => el === currValue)
+
+//       if (valueIsPlayed) {
+//         const rowWin = gridHasWinnerRow(accGrid)
+//         const columnWin = gridHasWinnerRow(convertColumnsToRows(accGrid))
+//         console.log("rowWin", rowWin, currValue)
+//         console.log("columnWin", columnWin, currValue)
+//         if (rowWin || columnWin) justCalledValue = currValue
+//       }
+
+//       // prepare markvalue
+//       const markValue = (value: string) => (value.endsWith("**") ? value : `${value}**`)
+//       return [...accValue, valueIsPlayed ? markValue(currValue) : currValue]
+//     }, [])
+
+//     return [...accGrid, markedRow]
+//   }, [])
+
+//   return { grid: newGrid, lastCalledValue: justCalledValue }
+// }
+
+/**
+ * Return true if each value of a row is marked
+ * @param grid
+ */
+export const gridHasWinnerRow = (grid: TGrid): boolean =>
+  grid.some((row: TRow) => row.every((value: string) => value.endsWith("**")))
+
+/**
+ * Get columns from a grid
+ * @param grid
+ */
+export const convertColumnsToRows = (grid: TGrid): TGrid => {
+  const valuesArray = []
+  for (let row of grid) {
+    row.forEach((value, i) => {
+      valuesArray[i] = [...(valuesArray[i] || []), value]
+    })
+  }
+  return valuesArray
 }
+
+// prepare markvalue
+const markValue = (value: string) => (value.endsWith("**") ? value : `${value}**`)
 
 // -------------------------------------------------------------------------- FINAL
 /**
@@ -46,16 +87,97 @@ export const markValuesOfOneGrid = (grid: TGrid, playValues: TPlayValues): TGrid
  * @return {boolean} return true if one column or one row of the grid is complete
  * (all numbers of this column or this row is marked)
  */
-export const parseOnGrid = (grid: TGrid, playValues: TPlayValues): boolean => {
-  // Mark
-  const markedGrid = markValuesOfOneGrid(grid, playValues)
-  // parse Row and Columns
+export const parseOneGrid = (grid: TGrid, playValues: TPlayValues): boolean => {
+  let lastCalledPlayValue: string = null
+  let newGrid: TGrid
 
-  console.log(markedGrid)
+  // for each play value
+  for (const playValue of playValues) {
+
+    const tempGrid = (newGrid || grid).reduce<TGrid>(
+      (accGrid: TGrid, currRow: TRow): TGrid => {
+       
+        // map on row values
+        const markedRow = currRow.reduce<TRow>(
+          (accValue: TRow, currValue: string): TRow => {
+            // check if current value is played
+            const valueIsPlayed = playValue === currValue
+
+            if (valueIsPlayed) {
+              const rowWin = gridHasWinnerRow(accGrid)
+              console.log("rowWin", {rowWin, currValue, accGrid})
+
+              const columnWin = gridHasWinnerRow(convertColumnsToRows(accGrid))
+              console.log("columnWin", columnWin, currValue)
+
+              if (rowWin || columnWin) {
+                console.log("row or column win", { currValue, playValue })
+                lastCalledPlayValue = currValue
+              }
+            }
+
+            return [...accValue, valueIsPlayed ? markValue(currValue) : currValue]
+          },
+           []
+        )
+        //console.log("markedRow",markedRow)
+        return [...(accGrid), markedRow]
+      },
+      []
+    )
+    console.log("tempGrid", tempGrid)
+    if (lastCalledPlayValue == null) {
+      newGrid = tempGrid
+    }
+  }
+
+  console.log("lastCalledPlayValue", lastCalledPlayValue)
+  console.log("newGrid", newGrid)
+
+  //   const markedGrid = grid.reduce<TGrid>((accGrid: TGrid, currRow: TRow): TGrid => {
+  //     // marked each value of of current row
+  //     const markedRow = currRow.reduce<TRow>((accValue: TRow, currValue: string): TRow => {
+  //       // check if current value is played
+  //       const valueIsPlayed = playValues.some((el) => el === currValue)
+
+  //       // prepare markvalue
+  //       const markValue = (value: string) => (value.endsWith("**") ? value : `${value}**`)
+  //       const newGrid = [...accValue, valueIsPlayed ? markValue(currValue) : currValue]
+
+  //       // if value is played and row or column win, register the winner value
+  //       if (valueIsPlayed) {
+  //         const rowWin = gridHasWinnerRow(accGrid)
+  //         console.log("rowWin", rowWin, currValue)
+
+  //         const columnWin = gridHasWinnerRow(convertColumnsToRows(accGrid))
+  //         console.log("columnWin", columnWin, currValue)
+
+  //         if (rowWin || columnWin) {
+  //           console.log("row or column win")
+  //           lastCalledPlayValue = currValue
+  //           return newGrid
+  //         }
+  //       }
+
+  //       return newGrid
+  //     }, [])
+
+  //     return [...accGrid, markedRow]
+  //   }, [])
+
+  // parse Row and Columns
+  //   const rowWin = gridHasWinnerRow(markedGrid)
+  //   const columnWin = gridHasWinnerRow(convertColumnsToRows(markedGrid))
+  //   console.log("rowWin", rowWin)
+  //   console.log("columnWin", columnWin)
 
   return
 }
 
+/**
+ *
+ * @returns
+ */
 export const parseAllGrids = (): boolean => {
   return
 }

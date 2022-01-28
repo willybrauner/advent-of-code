7644a7048a10e07549a24a6caecf5b4096bf4a52
Willy Brauner <mail@willybrauner.com>
2022-01-28 23:19:27 +0100

    Init

---
+++ src/2021/day-06/formatInputs.ts
@@ -0,0 +1,5 @@
+const fs = require("fs")
+const path = require("path")
+
+export default (filename: string = "inputs.txt") =>
+  fs.readFileSync(path.resolve(__dirname, filename), "utf8")
---
+++ src/2021/day-06/index.ts
@@ -0,0 +1,1 @@
+// https://adventofcode.com/2021/day/6

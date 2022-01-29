b4e6961f451177e32da3581a60d94caacfc8a891
Willy Brauner <mail@willybrauner.com>
2022-01-30 00:36:10 +0100

    day-6 part-2

--- src/2021/day-06/index.test.ts
+++ src/2021/day-06/index.test.ts
@@ -1,35 +1,24 @@
 import { part1, part2 } from "."
 import formatInputs from "./formatInputs"
 
-it("", () => {
+it("Part 1 example", () => {
   const inputs = formatInputs("inputs-example.txt")
-
-  const p1_18 = part1(inputs, 18)
-  expect(p1_18).toEqual([
-    6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8,
-  ])
-  expect(p1_18.length).toBe(26)
-
-  const p1_80 = part1(inputs, 80)
-  expect(p1_80.length).toBe(5934)
+  expect(part1(inputs, 18)).toBe(26)
+  expect(part1(inputs, 80)).toBe(5934)
 })
 
-it("", () => {
+it("Part 1", () => {
   const inputs = formatInputs("inputs.txt")
-  const p1 = part1(inputs, 80)
-  expect(p1.length).toBe(362346)
+  expect(part1(inputs, 80)).toBe(362346)
 })
 
-it("", () => {
-    const inputs = formatInputs("inputs-example.txt")
-    expect(part2(inputs, 18).length).toBe(26)
-    
-  //   const p1_256 = part1(inputs, 256)
-  //   console.log('p1_256',p1_256)
+it("Part 2 example", () => {
+  const inputs = formatInputs("inputs-example.txt")
+  expect(part2(inputs, 18)).toBe(26)
+  expect(part2(inputs, 256)).toBe(26984457539)
+})
 
-  // 18  -> 26
-  // 80  -> 5934
-  // 256 -> 26984457539
-
-  //expect(p1_256.length).toBe(362346)
+it("Part 2", () => {
+  const inputs = formatInputs("inputs.txt")
+  expect(part2(inputs, 256)).toBe(1639643057051)
 })
--- src/2021/day-06/index.ts
+++ src/2021/day-06/index.ts
@@ -1,13 +1,11 @@
 // https://adventofcode.com/2021/day/6
 
-export const part1 = (inputs: number[], days = 80): number[] => 
+export const part1 = (inputs: number[], days = 80): number => 
 {
-  
   const ipt = [...inputs]
 
   for (let d = 0; d < days; d++) 
   {
-
         for(let i in ipt)
         {
             if ( ipt[i] === 0 )
@@ -19,30 +17,26 @@ export const part1 = (inputs: number[], days = 80): number[] =>
             {
                 ipt[i] -= 1;
             }
-        }
-        
+        }  
   }
-    return ipt
-
+  return ipt.length
 }
 
 
-export const part2 = (inputs: number[], days = 256): number[] => 
+export const part2 = (inputs: number[], days = 256): number => 
 {
     const ipt = [...inputs]
     const groups :number[] = new Array(9).fill(0)
-    
+        
     ipt.forEach((e) => groups[e] += 1);
-    console.log("groups", groups)
 
-
-    // TODO
-  for (let d = 0; d < days; d++) 
-  {
-
-    
-  }
+    for (let d = 0; d < days; d++) 
+    {
+        const newFish = groups.shift();
+        groups[6] += newFish
+        groups.push(newFish)
+    }
   
-  return ipt
+    return groups.reduce((a, b) => a + b, 0)
 
 }

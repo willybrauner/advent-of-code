c05a002ffde83bd73bfd62f325b429ad60d9e913
Willy Brauner <mail@willybrauner.com>
2022-02-13 23:28:52 +0100

    Resolve day-7 part-1

--- src/2021/day-07/index.test.ts
+++ src/2021/day-07/index.test.ts
@@ -3,7 +3,5 @@ import formatInputs from "./formatInputs"
 
 it("Part 1 example", () => {
   const inputs = formatInputs("inputs-example.txt")
-  //console.log("inputs",inputs)
    expect(part1(inputs)).toBe(37)
-  
 })
--- src/2021/day-07/index.ts
+++ src/2021/day-07/index.ts
@@ -2,35 +2,20 @@
 
 
 export const part1 = (inputs: number[]) =>
-{
-    // calc the input average
-    const average = inputs
-        .reduce((a, b) => a + b, 0) 
-        /
-        inputs.length
-        console.log("average", average)
-
-    // get most frequent element in input array
-    const array = inputs
-        .reduce((a, b) => {
-            a[b] = (a[b] ?? 0) + 1
-            return [...a]
-        }, [])
-        .map(el => el ? el : 0)
-     console.log(array);
-
-     let wIndex = 0;
-     for (let i = 0; i <= array.length; i++) {
-        if (array[i] > array[wIndex]) wIndex = i
-     }
-    console.log(wIndex)
+{  
+    // calc mediane
+    const sorted = inputs.sort((a, b) => a - b)
+    const half = Math.floor(sorted.length / 2)
+    const mediane = 
+        sorted.length % 2 === 0
+        ? (sorted[half - 1] + sorted[half]) / 2
+        : sorted[half]
 
     // final, calc fuel value
     let fuel = 0;
     for (let input of inputs) {
-        fuel += Math.abs(wIndex - input)
+        fuel += Math.abs(mediane - input)
     }
-
     return fuel
 
 }

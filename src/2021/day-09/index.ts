9ad6a730424aaac04b745cb522c2574f26814d8a
Willy Brauner <mail@willybrauner.com>
2022-03-10 15:58:40 +0100

    Create template

---
+++ src/2021/day-09/index.ts
@@ -0,0 +1,10 @@
+// https://adventofcode.com/2021/day/9
+
+export const part1 = (inputs) =>
+ {
+
+ }
+
+export const part2 = (inputs) => {
+
+}
---
+++ src/_template/index.ts
@@ -0,0 +1,10 @@
+// https://adventofcode.com
+
+export const part1 = (inputs) =>
+ {
+
+ }
+
+export const part2 = (inputs) => {
+
+}

bffa3232c6d492d79c95e1251e9caecdf0444e55
Willy Brauner <mail@willybrauner.com>
2022-04-07 00:11:34 +0200

    day-11 part-2

--- src/2021/day-11/index.ts
+++ src/2021/day-11/index.ts
@@ -1,84 +1,108 @@
 // https://adventofcode.com/2021/day/11
 const { log } = console
 
-export const part1 = (inputs: number[][]) => {
 
-  let flashes = 0;
-  let stepFlashCoords = [];
+let flashes = 0;
+let stepFlashCoords = [];
 
-  const runStep = () => 
+const _runStep = (inputs) => 
+{
+  for (let y = 0; y < inputs.length; y++) 
   {
-
-   for (let y = 0; y < inputs.length; y++) 
+    for (let x = 0; x < inputs[y].length; x++) 
     {
-      for (let x = 0; x < inputs[y].length; x++) 
-      {
-        if (inputs[y][x] > 9) 
-        {  
-  
-          const adjCoor = [
-            [y-1, x-1],
-            [y-1, x],
-            [y-1, x+1],
-            [y, x-1],
-            [y, x+1],
-            [y+1, x-1],
-            [y+1, x],
-            [y+1, x+1] 
-          ]
-          
-          // flash!
-          inputs[y][x] = 0
-          
-          // if has aready flashed, next iteration
-          if (stepFlashCoords.some(e=> e.y === y && e.x === x)) continue
+      if (inputs[y][x] > 9) 
+      {  
 
-          // increment counter
-          flashes++
-          
-          // keep flash coordinates in global
-          stepFlashCoords.push({y,x})
-    
-          // increment adjcent
-           adjCoor.forEach((e) => {
-            if (!inputs?.[e[0]]?.[e[1]]) return
-            inputs[e[0]][e[1]]++
-          })
-
-          // if one of adjacent is gretter than 9, run step recucively
-          adjCoor.forEach((e) => {
-            if (inputs?.[e[0]]?.[e[1]] > 9) {
-              runStep()
-            }
-          })
+        const adjCoor = [
+          [y-1, x-1],
+          [y-1, x],
+          [y-1, x+1],
+          [y, x-1],
+          [y, x+1],
+          [y+1, x-1],
+          [y+1, x],
+          [y+1, x+1] 
+        ]
         
-        }
+        // flash!
+        inputs[y][x] = 0
+        
+        // if has aready flashed, next iteration
+        if (stepFlashCoords.some(e=> e.y === y && e.x === x)) continue
+
+        // increment counter
+        flashes++
+        
+        // keep flash coordinates in global
+        stepFlashCoords.push({y,x})
+  
+        // increment adjcent
+          adjCoor.forEach((e) => {
+          if (!inputs?.[e[0]]?.[e[1]]) return
+          inputs[e[0]][e[1]]++
+        })
+
+        // if one of adjacent is gretter than 9, run step recucively
+        adjCoor.forEach((e) => {
+          if (inputs?.[e[0]]?.[e[1]] > 9) {
+           _runStep(inputs)
+          }
+        })
+      
       }
     }
   }
+}
 
 
-  //  start !!!!!
+export const part1 = (inputs: number[][]) => {
+
+    //  start !
   for (let i = 0 ; i < 100; i++)
   {
     // re init array
     stepFlashCoords = []
     // increment each 
-    inputs = inputs.map(e => e.map(e => e+1))    
+    inputs = inputs.map(e => e.map(e => e+1)) 
     // ruuuuun
-    runStep()
-
+   _runStep(inputs)
   }
   
-  return flashes
+  // resolve part 1
+   return flashes
+
+
 }
 
+/**
+ * Part 2
+ * @param inputs 
+ */
+export const part2 = (inputs: number[][]) => 
+{
 
+  let fullFlashingMotherFunkingStepNumber = 0
+  while(true)
+  {
 
+    fullFlashingMotherFunkingStepNumber++
 
+    // re init array
+    stepFlashCoords = []
+    // increment each 
+    inputs = inputs.map(e => e.map(e => e+1)) 
+    // ruuuuun
+   _runStep(inputs)
 
+    
 
+    if (inputs.every(e => e.every(e => e === 0)))
+    {
+      break
+    }
+  }
 
+  return fullFlashingMotherFunkingStepNumber
 
-
-export const part2 = (inputs) => {}
+}

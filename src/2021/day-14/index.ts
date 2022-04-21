4bc17216f1327876b3f4184bb3098d2d49b8adb2
Willy Brauner <mail@willybrauner.com>
2022-04-21 20:27:10 +0200

    part-1

--- src/2021/day-14/index.ts
+++ src/2021/day-14/index.ts
@@ -1,10 +1,77 @@
-// https://adventofcode.com
+// https://adventofcode.com/2021/day/14
 
-export const part1 = (inputs) =>
+import { TInputs } from "./inputs-format"
+const {log} = console
+
+
+export const part1 = ([template, pairIntersections]: TInputs) =>
  {
+    //log(template, pairIntersections)
+    const tmpls: string[] = [template]
+    
+    let step = 0
+    while(step < 4)
+    {
+        const tmpl = tmpls[tmpls.length - 1]
+        const tmplArr = tmpl.split("");
+        log("------------- tmpl",tmpl)
+        
+        for(let i = 0; i < tmpl.length - 1; i++)
+        {
+            
+            const currTemplatePair = tmpl.slice(i , 2+i);
+            log("currTemplatePair",currTemplatePair)
+            
+            // parse pair instructions array 
+            for(let pairInter of pairIntersections)
+            {
+                // if pair instructionnion = 
+                if (pairInter[0] === currTemplatePair)
+                {   
+       
+                    log("startingTemplate", tmplArr)
+
+                    let prev = ""
+                    for (let p = 0; p < tmplArr.length; p++)
+                    {
+                        //const realPrev = prev.split("")[prev.split("").length - 1]
+
+                        if (
+                            pairInter[0][0] === prev[0] && 
+                            pairInter[0][1] === tmplArr[p][0]
+                            )
+                        {
+        
+                            // insert             
+                            log("pairInter[1]",pairInter[1])           
+                            tmplArr[i] = tmplArr[i][0] + pairInter[1]
+                            log('startingTemplate --',tmplArr)
+                            
+                            //push insert in debut array
+                            tmpls.push(tmplArr.join(''))
+
+                        }
+                        
+                        prev = tmplArr[p]
+                    }
+                }
+            }
+        }
+
+        
+        log("step", step)
+        log("tmpls", tmpls)
+        log("tmpls last", tmpls[tmpls.length - 1])
+        // NCNBCHB
+        step++
+    }
+
+
+    
 
  }
 
 export const part2 = (inputs) => {
 
 }
+ 
--- src/2021/day-14/inputs-format.ts
+++ src/2021/day-14/inputs-format.ts
@@ -1,5 +1,20 @@
 const fs = require("fs")
 const path = require("path")
 
-export default (filename: string = "inputs.txt") =>
+export type TTemplate = string
+export type TPair = [string, string]
+export type TInputs = [TTemplate, TPair]
+
+export default (filename: string = "inputs.txt"): TInputs =>
   fs.readFileSync(path.resolve(__dirname, filename), "utf8")
+  .split("\n").filter(e => e)
+  .reduce((a, b, i) =>  
+  {
+    const arr = a;
+    if (i === 0) 
+      arr[0] = b
+    else 
+      arr[1].push(b.split(' -> '))
+    return arr
+  }
+  ,["", []])

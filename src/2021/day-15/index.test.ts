4180a262575b3f9100d0137fc570ae3dab986a67
Willy Brauner <mail@willybrauner.com>
2022-12-02 00:52:51 +0100

    Update template

--- src/2021/day-15/index.test.ts
+++ src/2021/day-15/index.test.ts
@@ -4,12 +4,10 @@ const {log} = console
 
 it("Part 1 example", () => {
   const inputs = formatInputs("inputs.txt")
-   console.log( part1(inputs) )
    // expect(part1(inputs)).toBe(527)
 })
 
 it("Part 2 example", () => {
   const inputs = formatInputs("inputs.txt")
-  console.log(part2(inputs))
   // expect(part2(inputs)).toBe(2887)
 })
--- src/_template/index.test.ts
+++ src/_template/index.test.ts
@@ -1,16 +1,14 @@
-import { part1, part2 } from "./index"
-import formatInputs from "./inputs-format"
-const {log} = console
+import { format, part1, part2 } from "./index"
+const { log } = console
 
-it("Part 1 example", () => {
-  const inputs = formatInputs("inputs-example.txt")
-  log(inputs)
-  part1(inputs)
-  //expect(part1(inputs)).toBe(...)
+it("resolve part 1", () => {
+  const input = format("input.test.txt")
+  part1(input)
+  // expect(part1(input)).toBe( )
 })
 
-it("Part 2 example", () => {
-  const inputs = formatInputs("inputs-example.txt")
-  part2(inputs)
-  //expect(part2(inputs)).toBe(...)
+it("resolve part 2", () => {
+  const input = format("input.test.txt")
+  part2(input)
+  //expect(part2(input)).toBe( )
 })

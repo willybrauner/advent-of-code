6f9e81d0fbb528769c41f2b66c3dc393a864bc96
Willy Brauner <mail@willybrauner.com>
2022-12-02 01:45:28 +0100

    Move src content on root

---
+++ 2022/day-02/index.test.ts
@@ -0,0 +1,14 @@
+import { format, part1, part2 } from "./index"
+const { log } = console
+
+it("resolve part 1", () => {
+  const input = format("input.test.txt")
+  part1(input)
+  // expect(part1(input)).toBe( )
+})
+
+it("resolve part 2", () => {
+  const input = format("input.test.txt")
+  part2(input)
+  //expect(part2(input)).toBe( )
+})
---
+++ 2022/day-02/index.ts
@@ -0,0 +1,19 @@
+// https://adventofcode.com/2022/day/2
+import fs from "fs"
+import path from "path"
+const { log } = console
+
+export type TInput = any
+
+export const format = (filename: "input.test.txt" | "input.txt"): TInput =>
+  fs.readFileSync(path.resolve(__dirname, filename), "utf8")
+
+export const part1 = (input: TInput) =>
+{
+  log("input", input)
+}
+
+export const part2 = (input: TInput) =>
+{
+
+}

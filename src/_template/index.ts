a937acb31ef4bce30f1ea664e3e7c5110e28b122
Willy Brauner <mail@willybrauner.com>
2022-12-02 01:28:30 +0100

    Update template (#13)

--- src/_template/index.ts
+++ src/_template/index.ts
@@ -1,10 +1,19 @@
-// https://adventofcode.com
+// https://adventofcode.com/2022/day/
 
-export const part1 = (inputs) =>
- {
+import fs from "fs"
+import path from "path"
+const { log } = console
 
- }
+export type TInput = any
+export const format = (filename: "input.test.txt" | "input.txt"): TInput =>
+  fs.readFileSync(path.resolve(__dirname, filename), "utf8")
 
-export const part2 = (inputs) => {
+export const part1 = (input: TInput) =>
+{
+  log(input)
+}
+
+export const part2 = (input: TInput) =>
+{
 
 }

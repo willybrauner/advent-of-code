18d0e5766af84dea63f6033ae8cc78de783a6af5
Willy Brauner <mail@willybrauner.com>
2022-12-04 09:49:15 +0100

    resolve

--- 2022/day-04/index.ts
+++ 2022/day-04/index.ts
@@ -7,16 +7,10 @@ import fs from 'fs'
 import path from 'path'
 const { log } = console
 
-export type TInput = string[][]
+export type TInput = Set<any>[][]
 
 /**
- * Return this struct
- [
-   [ '2-4', '6-8' ],
-   [ '2-3', '4-5' ],
-   [ '5-7', '7-9' ],
-   ...
- ]
+ * Return Sets of numbers
  */
 export const format = (filename: 'input.test.txt' | 'input.txt'): TInput =>
   fs
@@ -24,23 +18,22 @@ export const format = (filename: 'input.test.txt' | 'input.txt'): TInput =>
     .split('\n')
     .filter((e) => e)
     .map((e) => e.split(','))
+    .map((e) =>
+      // convert: [ '2-4', '6-8' ]
+      // to: Set(3) { 2, 3, 4 } Set(3) { 6, 7, 8 }
+      e.reduce((prev: Set<any>[], curr: string): Set<any>[] => {
+        const nums = curr.split('-').map((e) => parseInt(e))
+        const suite = new Set()
+        for (let i = nums[0]; i <= nums[1]; i++) suite.add(i)
+        return [...prev, suite]
+      }, [])
+    )
 
 /**
  * Part 1
  */
 export const part1 = (input: TInput = format('input.test.txt')) =>
-  input.reduce((a, b) => {
-    // convert: [ '2-4', '6-8' ] to: Set(3) { 2, 3, 4 } Set(3) { 6, 7, 8 }
-    const [set1, set2] = b.reduce(
-      (prev: Set<any>[], curr: string): Set<any>[] => {
-        const nums = curr.split('-').map((e) => parseInt(e))
-        const suite = new Set()
-        for (let i = nums[0]; i <= nums[1]; i++) suite.add(i)
-        return [...prev, suite]
-      },
-      []
-    )
-    //
+  input.reduce((a, [set1, set2]) => {
     const intersectSet = new Set([...set1].filter((x) => set2.has(x)))
     const match =
       intersectSet.size === set1.size || intersectSet.size === set2.size
@@ -51,18 +44,7 @@ export const part1 = (input: TInput = format('input.test.txt')) =>
  * part2
  */
 export const part2 = (input: TInput = format('input.test.txt')) =>
-  input.reduce((a, b) => {
-    // convert: [ '2-4', '6-8' ] to: Set(3) { 2, 3, 4 } Set(3) { 6, 7, 8 }
-    const [set1, set2] = b.reduce(
-      (prev: Set<any>[], curr: string): Set<any>[] => {
-        const nums = curr.split('-').map((e) => parseInt(e))
-        const suite = new Set()
-        for (let i = nums[0]; i <= nums[1]; i++) suite.add(i)
-        return [...prev, suite]
-      },
-      []
-    )
-    //
+  input.reduce((a, [set1, set2]) => {
     const intersectSet = new Set([...set1].filter((x) => set2.has(x)))
     return a + (intersectSet.size ? 1 : 0)
   }, 0)

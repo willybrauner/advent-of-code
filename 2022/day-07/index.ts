00db0eb5da1b49790cb2d5e55399a8104fba845a
Willy Brauner <mail@willybrauner.com>
2022-12-07 22:34:42 +0100

    WIP

--- 2022/day-07/index.ts
+++ 2022/day-07/index.ts
@@ -50,93 +50,90 @@ export const format = (filename: 'input.test' | 'input'): TInput => {
 
   const FINAL = {}
 
-  const getParentObject = (obj, full = FINAL) =>
-  {
-
-  }
-
-  const read = (lines = browse, tree = FINAL, parentLevel = null) => {
+  const read = (lines = browse, tree = FINAL, path = '') => {
     for (let i = 0; i < lines.length; i++) {
-      //  if (i > 1) return
       const line = lines[i]
 
-      // prettier-ignore
-      if (line[0] === '$')
-      {
+      if (line[0] === '$') {
         if (line.split(' ')[1] === 'cd') {
-            // ROOT (only one root / and called once
-            if ( line.split(" ")[2] === "/") {
-              Object.assign(tree,{ "/": {} })
-              lines.shift()
-              return read(lines, tree['/'], tree)
-            }
-            // cd .. is back
-            else if (line.split(' ')[2] === '..') {
-              lines.shift()
-              log("COMMAND = '..'",parentLevel)
-              // FIXME ici on devrait passer l'objet parent
-                // pour se faire il faudrait parser l'objet FINAL
-              // et trouver l'obj qui fait égalité avec
-              // ou tout le temps garder le chemin
-               return read(lines, tree)
-
-            }
-            // Enter in folder ex: "cd a"
-            else {
-              const key = line.split(' ')[2];
-              // log('line ------------------', line)
-              lines.shift()
-              return read(lines, tree[key], tree)
-            }
+          // "$ cd /" ROOT
+          if (line.split(' ')[2] === '/') {
+            Object.assign(tree, { '/': {} })
+            lines.shift()
+            return read(lines, tree['/'], path + '/')
+          }
+          // "$ cd .." is back, return parent object from path
+          else if (line.split(' ')[2] === '..') {
+            lines.shift()
+            const parentPath = path.slice(0, path.length - 1)
+            const parentPathArr = parentPath.split('')
+            let parentTree = FINAL
+            for (let entry of parentPathArr) parentTree = parentTree[entry]
+            return read(lines, parentTree, parentPath)
+          }
+          // Enter in folder ex: "cd a"
+          else {
+            const key = line.split(' ')[2]
+            lines.shift()
+            return read(lines, tree[key], path + key)
+          }
         }
-        // show
+        // Juste show
         else if (line.split(' ')[1] === 'ls') {
-          // TODO
-          // log('COMNAND = $ ls', line)
           lines.shift()
-          return read(lines, tree, parentLevel)
-          // return read(lines, tree)
+          return read(lines, tree, path)
         }
-
       }
       // read: dir (restart on this recursively)
       else if (line.split(' ')[0] === 'dir') {
-        const key = line.split(' ')[1];
-        Object.assign(tree,{ [key]: {} })
-        //log('tree-------',tree)
+        const key = line.split(' ')[1]
+        Object.assign(tree, { [key]: {} })
         lines.shift()
-        return read(lines, tree, parentLevel)
+        return read(lines, tree, path)
       }
-      // FINAL of this obj
+
       // read: Number (is the file size
       else if (typeof parseInt(line.split(' ')[0]) === 'number') {
         lines.shift()
-
-        if (line.split(' ')[1])
-        {
-          Object.assign(tree, { [line.split(' ')[1]]: parseInt(line.split(' ')[0]) })
-          return read(lines, tree, parentLevel)
+        const key = line.split(' ')[1]
+        if (key) {
+          Object.assign(tree, {
+            [key]: parseInt(line.split(' ')[0]),
+          })
+          return read(lines, tree, path)
         }
-
-
       }
-
-      // if (!lines.length) return tree
     }
-
     return FINAL
   }
 
   const tree = read()
-  log('tree', tree)
-  log("tree['/']['a']['e']", tree['/']['a']['e'])
+  return tree
+}
+
+const flatDirectories = (input) => {
+  const FINAL = []
+  const parser = (entries) => {
+    for (let entry of Object.keys(entries)) {
+//      log('entry',entry)
+      // if (typeof entry === 'object') {
+      //   log('entry', entry)
+      // }
+    }
+  }
+  parser(input)
 }
 
 /**
  * Part 1
  */
 export const part1 = (input: TInput = format('input.test')) => {
-  return input
+  log(input)
+  const bla = flatDirectories(input)
+
+  // check dans
+
+  //  return input
 }
 
 /**

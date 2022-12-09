973b6ae3f1067bb92f19fe181158a687906c1472
Willy Brauner <mail@willybrauner.com>
2022-12-09 01:12:55 +0100

    Create template

---
+++ 2022/day-09/index.ts
@@ -0,0 +1,31 @@
+/**
+ * 2022 - Day 09
+ *  https://adventofcode.com/2022/day/9
+ */
+
+import fs from 'fs'
+import path from 'path'
+const { log, clear } = console
+clear()
+
+export type TInput = any
+
+export const format = (filename: 'input.test' | 'input'): TInput =>
+  fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
+
+/**
+ * Part 1
+ */
+export const part1 = (input: TInput) => {
+  log(input)
+  return input
+}
+part1(format('input.test'))
+
+/**
+ * part2
+ */
+export const part2 = (input: TInput) => {
+  return input
+}
+part2(format('input.test'))

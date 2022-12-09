d017589b97bcf8aebde56b6e7cf9aae49331eb66
Willy Brauner <mail@willybrauner.com>
2022-12-09 12:45:15 +0100

    WIP

--- 2022/day-09/index.ts
+++ 2022/day-09/index.ts
@@ -8,16 +8,53 @@ import path from 'path'
 const { log, clear } = console
 clear()
 
-export type TInput = any
+export type TInput = ['R' | 'U' | 'L' | 'D', number][]
 
 export const format = (filename: 'input.test' | 'input'): TInput =>
-  fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter((e) => e)
+    .map((e) =>
+      e.split(' ').map((e, i) => (i == 1 ? parseInt(e) : e))
+    ) as TInput
 
 /**
  * Part 1
  */
 export const part1 = (input: TInput) => {
-  log(input)
+  const T = [{ x: 0, y: 0 }]
+  let H = { x: 0, y: 0 }
+
+  for (let i = 0; i < input.length; i++) {
+     if (i > 3) return
+    const [dir, move] = input[i]
+    log('-'.repeat(20))
+    log({ dir, move })
+    log('-'.repeat(20))
+
+    for (let m = 0; m < move; m++)
+    {
+      const isLastMove = m === move - 1
+      if (dir == 'R') {
+        H = { x: H.x + 1, y: H.y }
+      }
+      if (dir == 'U') {
+        H = { x: H.x, y: H.y - 1 }
+      }
+      if (dir == 'L') {
+        H = { x: H.x - 1, y: H.y }
+      }
+      if (dir == 'D') {
+        H = { x: H.x, y: H.y + 1 }
+      }
+      if (!isLastMove) T.push(H)
+    }
+
+    log({ T, lastH: H })
+  }
+
+  log(new Set(T).size)
   return input
 }
 part1(format('input.test'))

b33a3d8a8debf8d6a8649299ea686b5344481369
Willy Brauner <mail@willybrauner.com>
2022-12-10 21:09:29 +0100

    Part 2

--- 2022/day-10/index.ts
+++ 2022/day-10/index.ts
@@ -21,8 +21,6 @@ export const format = (filename: 'input.test' | 'input'): TInput =>
  * Part 1
  */
 export const part1 = (input: TInput) => {
-  // (input)
-
   let x = 1
   let cycles = 0
   let strength = 0
@@ -30,11 +28,7 @@ export const part1 = (input: TInput) => {
   const incrementCycles = () => {
     cycles++
     for (let cycle of [20, 60, 100, 140, 180, 220]) {
-      if (cycles === cycle) {
-        log(`is ${cycle} !, how many is X value ?`, x)
-        strength += cycle * x
-        log('No strength is', strength)
-      }
+      if (cycles === cycle) strength += cycle * x
     }
   }
 
@@ -48,7 +42,6 @@ export const part1 = (input: TInput) => {
       }
     }
   }
-
   return input
 }
 part1(format('input.test'))
@@ -57,6 +50,46 @@ part1(format('input.test'))
  * part2
  */
 export const part2 = (input: TInput) => {
-  
+  let x = 1
+  let cycles = 0
+
+  let sprite = [0, 1, 2]
+  const moveSprite = (pos) =>
+    (sprite = new Array(3).fill(null).map((e, i) => pos + i))
+  const matchWithSprite = (v) => sprite.some((e) => e === v)
+
+  let CRTPos = 0
+  let CRT = ''
+  const CRTS = []
+
+  const incrementCycles = () => {
+    cycles++
+    CRTPos++
+    CRT += matchWithSprite(CRTPos) ? '#' : '.'
+    const checks = [40, 80, 120, 160, 200, 240]
+    for (let i = 0; i < checks.length; i++) {
+      if (cycles === checks[i]) {
+        log(CRT) // display response \o\
+        CRTS.push(CRT)
+        CRT = ''
+        CRTPos = 0
+      }
+    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    let curr = input[i]
+    if (curr === 0) incrementCycles()
+    else {
+      for (let c = 0; c < 2; c++) {
+        incrementCycles()
+        if (c === 1) {
+          x += curr
+          moveSprite(x)
+        }
+      }
+    }
+  }
+  return input
 }
-part2(format('input.test'))
+part2(format('input'))

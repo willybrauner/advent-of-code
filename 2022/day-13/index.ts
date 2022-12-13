1331b648e5d5b62a501373c4dcff98ad9292656b
Willy Brauner <mail@willybrauner.com>
2022-12-14 00:45:06 +0100

    Start part 1

--- 2022/day-13/index.ts
+++ 2022/day-13/index.ts
@@ -1,31 +1,73 @@
 /**
- * XXXX - Day X
- *  https://adventofcode.com/2022/day/X
+ * 2022 - Day 13
+ *  https://adventofcode.com/2022/day/13
  */
-
 import fs from 'fs'
 import path from 'path'
+import retryTimes = jest.retryTimes
 const { log, clear } = console
 clear()
 
-export type TInput = any
+type Side = number[]
+type Pair = Side[]
+type Input = [Pair, Pair]
 
-export const format = (filename: 'input.test' | 'input'): TInput =>
-  fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
+const format = (filename: 'input.test' | 'input'): Input =>
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .toString()
+    .split('\n\n')
+    .map((e) =>
+      e
+        .split('\n')
+        .filter((e) => e)
+        .map((e) => eval(e))
+    ) as Input
+
+const arraysAreEqual = (a, b): boolean => {
+  if (a.length !== b.length) return false
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false
+  }
+  return true
+}
+
+// prettier-ignore
+const compare = ([L, R]: Pair): boolean | undefined => {
+  const arrL: boolean = Array.isArray(L)
+  const arrR: boolean = Array.isArray(R)
+  // Do comparison
+  if (arrL && arrR)
+    if (arraysAreEqual(L, R)) return true
+    if (L.length === 0) return
+    if (R.length === 0) return true
+
+  else if (arrL && !arrR) {}
+  else if (!arrL && arrR) {}
+  else {
+    if (L < R) {}
+    else if (L > R) {}
+    else return
+  }
+}
 
 /**
  * Part 1
  */
-export const part1 = (input: TInput) => {
-  log(input)
-  return input
+const part1 = (input: Input) => {
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const isInOrder = compare(input[i])
+    if (isInOrder) count += i + 1
+  }
+  return count
 }
-part1(format('input.test'))
+log(part1(format('input.test')))
 
 /**
  * part2
  */
-export const part2 = (input: TInput) => {
+const part2 = (input: Input) => {
   return input
 }
 part2(format('input.test'))

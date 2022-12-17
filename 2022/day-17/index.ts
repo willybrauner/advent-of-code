061be4164e857bfa8f6def613e5c15551d57275d
Willy Brauner <mail@willybrauner.com>
2022-12-17 18:00:03 +0100

    WIP

--- 2022/day-17/index.ts
+++ 2022/day-17/index.ts
@@ -10,42 +10,6 @@ clear()
 
 export type TInput = string[]
 
-export const format = (filename: 'input.test' | 'input'): TInput =>
-  fs
-    .readFileSync(path.resolve(__dirname, filename), 'utf8')
-    .split('\n')
-    .filter((e) => e)[0]
-    .split('')
-
-// prettier-ignore
-const rocks = [
-  [
-    [0, 0], [1, 0], [2, 0], [3, 0]
-  ],
-  [
-    null, [1, 0], null,
-    [0, 1],[1, 1],[2, 1],
-    null, [1, 2], null
-  ],
-  [
-     null, null, [2, 0],
-     null, null, [2, 1],
-    [0, 2],[1, 2],[2, 2]
-  ],
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-  ],
-  [
-    [0, 0], [1, 0],
-    [0, 1], [1, 1],
-  ],
-]
-const grid = []
-const GRID_WIDTH = 7
-
 const chunksArr = (input, size) =>
   input.reduce((a, b, i) => {
     return i % size === 0
@@ -57,42 +21,75 @@ export const clamp = (min: number, value: number, max: number): number => {
   return Math.max(min, Math.min(value, max))
 }
 
+export const format = (filename: 'input.test' | 'input'): TInput =>
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter((e) => e)[0]
+    .split('')
+
+// prettier-ignore
+const rocks = [
+  [
+    [2, 0],[3, 0],[4, 0],[5, 0]
+  ],
+  [
+           [3, 0],
+    [2, 1],[3, 1],[4, 1],
+           [3, 2]
+  ],
+  [
+                  [4, 0],
+                  [4, 1],
+    [2, 2],[3, 2],[4, 2]
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+    [2, 3],
+  ],
+  [
+    [2, 0], [3, 0],
+    [2, 1], [3, 1],
+  ],
+]
+
+
+const updateY = (rock) => rock.reduce((a, b) => [...a, [b[0], b[1]+1]], [])
+// prettier-ignore
+const updateX = (rock, pattern) => {
+  const smallestX = rock.reduce((a, b) => (b[0] < a ? b[0] : a), rock[0][0])
+  const biggestX = rock.reduce((a, b) => (b[0] > a ? b[0] : a), 0)
+  if (pattern === '>' && biggestX === 6) return rock
+  if (pattern === '<' && smallestX === 0) return rock
+  return rock.reduce((a, b, i) => [
+    ...a, [ b[0] + (pattern == '>' ? +1 : -1), b[1] ]
+  ], [])
+}
+
+const intersect = () => {}
+
 /**
  * Part 1
  */
 export const part1 = (patterns: TInput) => {
-  const playRock = (pattern, rock) => {
-
-
-    const updateRockPosition = () =>
-    {
-
-    }
-
-    // const biggestX = rock.reduce((a, b) => (b?.[0] > a ? b?.[0] : a), 0)
-    // const pos = { top: -3, left: 2, with: biggestX }
-    //   log('before', pattern, pos)
-
-      while (true)
-      {
-  //      if (pattern === '>')
-        //  pos.left = clamp(0, pos.left + 1, GRID_WIDTH - 1 - biggestX)
-//        if (pattern === '<')
-         // pos.left = clamp(0, pos.left - 1, GRID_WIDTH - 1 - biggestX)
-//        if (pos.top < 0) pos.top += 1
-      }
-
-      // log('after', pattern, pos)
-
-   // return pos
-  }
+  const played = []
 
   for (let i = 0; i < 2022; i++) {
-    //FIXME
-    log('------------')
     if (i > 1) return
     const rock = rocks[i % rocks.length]
-    playRock(patterns[i], rock)
+    const pattern = patterns.shift()
+
+    let curr = rock
+    let count = 0
+    //
+    while (count < 3) {
+      curr = updateX(curr, pattern)
+      curr = updateY(curr)
+      console.log('curr rock', curr)
+      count++
+    }
   }
   return patterns
 }

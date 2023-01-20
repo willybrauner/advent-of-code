d043fc3e33e551421a6f2fa6e9d66a46490cbc14
Willy Brauner <mail@willybrauner.com>
2023-01-20 16:59:16 +0100

    2022 - day-09 part 2 (#28)

--- 2022/day-09/index.ts
+++ 2022/day-09/index.ts
@@ -8,68 +8,79 @@ import path from 'path'
 const { log, clear } = console
 clear()
 
-export type TInput = ['R' | 'U' | 'L' | 'D', number][]
 
-export const format = (filename: 'input.test' | 'input'): TInput =>
+
+type Direction = 'R' | 'U' | 'L' | 'D';
+type Input = [Direction, number][]
+
+export const format = (filename: 'input.test' | 'input'): Input =>
   fs
     .readFileSync(path.resolve(__dirname, filename), 'utf8')
     .split('\n')
     .filter((e) => e)
     .map((e) =>
       e.split(' ').map((e, i) => (i == 1 ? parseInt(e) : e))
-    ) as TInput
+    ) as Input
+
+const resolve = (input: Input, rope: [number, number][]) => {
+  const visited = new Set()
+
+  const moveHead = (direction) => {
+    if (direction === 'U') rope[0][1]++
+    if (direction === 'D') rope[0][1]--
+    if (direction === 'L') rope[0][0]--
+    if (direction === 'R') rope[0][0]++
+  }
+
+  const moveTail = () => {
+    for (let i = 1; i < rope.length; i++) {
+      const [hx, hy] = rope[i - 1]
+      const [tx, ty] = rope[i]
+      const dx = hx - tx
+      const dy = hy - ty
+      if (Math.abs(dx) <= 1 && Math.abs(dy) <= 1) return
+      if (dy !== 0) rope[i][1] += dy > 0 ? 1 : -1
+      if (dx !== 0) rope[i][0] += dx > 0 ? 1 : -1
+    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    const [direction, move] = input[i]
+    for (let j = 0; j < move; j++) {
+      moveHead(direction)
+      moveTail()
+      visited.add(`${rope[rope.length - 1][0]}-${rope[rope.length - 1][1]}`)
+    }
+  }
+  return visited.size
+}
 
 /**
  * Part 1
  */
-export const part1 = (input: TInput) => {
-  const T = [{ x: 0, y: 0 }]
-  let prevH: { x: number; y: number }
-  let H = { x: 0, y: 0 }
-
-  for (let i = 0; i < input.length; i++) {
-    const [dir, move] = input[i]
-    for (let m = 0; m < move; m++) {
-      prevH = H
-      if (dir == 'R') H = { x: H.x + 1, y: H.y }
-      if (dir == 'U') H = { x: H.x, y: H.y - 1 }
-      if (dir == 'L') H = { x: H.x - 1, y: H.y }
-      if (dir == 'D') H = { x: H.x, y: H.y + 1 }
-      if (
-        ![
-          [0, 0],
-          [0, -1],
-          [1, -1],
-          [1, 0],
-          [1, 1],
-          [0, 1],
-          [-1, 1],
-          [-1, 0],
-          [-1, -1],
-        ].some(
-          (e) =>
-            JSON.stringify({
-              x: T[T.length - 1].x + e[0],
-              y: T[T.length - 1].y + e[1],
-            }) === JSON.stringify(H)
-        )
-      ) {
-        T.push(prevH)
-      }
-    }
-  }
-
-  return T.reduce(
-    (a, b) => (!a.some((e) => b.x === e.x && b.y === e.y) ? [...a, b] : a),
-    []
-  ).length
+export const part1 = (input: Input) => {
+  return resolve(input, [
+    [0, 0],
+    [0, 0],
+  ])
 }
-part1(format('input.test')) // 13
+log(part1(format('input.test')))
 
 /**
  * part2
  */
-export const part2 = (input: TInput) => {
-  return input
+export const part2 = (input: Input) => {
+  return resolve(input, [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+  ])
 }
-part2(format('input.test'))
+log(part2(format('input.test')))

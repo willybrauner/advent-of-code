e0b95145e8e10b37497e5ce98bc0b0c0d9637e51
Willy Brauner <willybrauner@gmail.com>
2023-07-29 11:03:45 +0200

    day-04

--- _template/index.ts
+++ _template/index.ts
@@ -13,19 +13,14 @@ export type Input = any
 export const format = (filename: 'input.test' | 'input'): Input =>
   fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
 
-/**
- * Part 1
- */
-export const part1 = (input: Input) => {
+
+const part1 = (input: Input) => {
   log(input)
   return input
 }
 part1(format('input.test'))
 
-/**
- * part2
- */
-export const part2 = (input: Input) => {
+const part2 = (input: Input) => {
   return input
 }
 part2(format('input.test'))

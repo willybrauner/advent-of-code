448397c24b6f55405f5550b9f13ac5443e83123a
Willy Brauner <willybrauner@gmail.com>
2023-12-01 11:15:48 +0100

    part-2

--- 2023/day-01/index.ts
+++ 2023/day-01/index.ts
@@ -4,41 +4,63 @@
  */
 
 import fs from 'fs'
-import path from 'path'
+import { resolve } from 'path'
 const { log, clear } = console
 clear()
 
-type Input = string[][]
-const format = (filename: 'input.test' | 'input'): Input =>
+const part1 = (filename) =>
   fs
-    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .readFileSync(resolve(__dirname, filename), 'utf8')
     .split('\n')
     .filter(Boolean)
     .map((e) => e.split(''))
+    .reduce((a: number, b: string[]) => {
+      const numbs = b.filter(Number)
+      const num = parseInt(numbs[0] + numbs[numbs.length - 1])
+      return a + num
+    }, 0)
 
-const part1 = (input: Input) =>
-  input.reduce((a: number, b: string[]) => {
-    const numbs = b.filter(Number)
-    const num = parseInt(numbs[0] + numbs[numbs.length - 1])
-    return a + num
-  }, 0)
+// log(part1('input'))
 
-log(part1(format('input')))
+const part2 = (filename) =>
+  fs
+    .readFileSync(resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .reduce((acc, curr, i) => {
+      // prettier-ignore
+      const strings = [
+       'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+       '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      ]
 
-const part2 = (input: Input) => {
-  const n = [
-    '',
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-  ];
+      const numbs = []
+      const recursive = (current) => {
+        for (let i = 0; i < strings.length; i++) {
+          const str = strings[i]
+          if (new RegExp(`^${str}`).test(current)) {
+            numbs.push(str)
+            const s = current.slice(str.length)
+            return recursive(s)
+          }
+        }
+        if (current.length) {
+          const s = current.slice(1)
+          return recursive(s)
+        }
+      }
 
-  return input
-}
-part2(format('input.test'))
+      recursive(curr)
+
+      const convertNumbs = numbs.map((e) =>
+        Number.isNaN(parseInt(e)) ? `${strings.indexOf(e) + 1}` : e,
+      )
+
+      const num = parseInt(
+        convertNumbs[0] + convertNumbs[convertNumbs.length - 1],
+      )
+
+      return acc + num
+    }, 0)
+
+log(part2('input'))

f33e6d2dfdb5c3e7db0a61a13231b6768f460193
Willy Brauner <willybrauner@gmail.com>
2023-12-03 14:53:38 +0100

    get data-struct

--- 2023/day-03/index.ts
+++ 2023/day-03/index.ts
@@ -6,15 +6,66 @@
 import fs from 'fs'
 import path from 'path'
 const { log, clear } = console
-clear()
 
-type Input = any
+type Coords = Record<string, string>
+type Symbols = Record<string, string>
+type Input = {
+  coords: Coords
+  symbols: Symbols
+}
+
+/**
+ Data struct
+ {
+  coords: {
+    '0,0': '467',
+    '0,5': '114'
+    ...
+  },
+  symbols: {
+    '1,3': '*',
+    '3,6': '#',
+    ...
+  }
+}
+
+ */
 const format = (filename: 'input.test' | 'input'): Input =>
-  fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
     .split('\n')
-    .map(e => e.split(""))
+    .filter(Boolean)
+    .reduce((acc: Input, curr: string, y: number) => {
+      const chars = curr.split('')
+      let coords = {}
+      let symbols = {}
+      let num = ''
+      let startX = -1
+      for (let x = 0; x < chars.length; x++) {
+        const char = chars[x]
+        if (!isNaN(parseInt(char))) {
+          if (num === '') startX = x
+          num += char
+        } else if (char === '.') {
+          if (num !== '') {
+            coords[`${y},${startX}`] = num
+            num = ''
+          }
+        } else {
+          symbols[`${y},${x}`] = char
+        }
+      }
+      // record last if curr num is not empty
+      if (num !== '') coords[`${y},${startX}`] = num
+      return {
+        coords: { ...acc.coords, ...coords },
+        symbols: { ...acc.symbols, ...symbols },
+      }
+    }, {} as Input)
 
 const part1 = (input: Input) => {
+  clear()
+
   return input
 }
 

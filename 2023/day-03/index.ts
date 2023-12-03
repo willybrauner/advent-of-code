cfe89ee1383fe2dc7623580cd8884c58b12c8846
Willy Brauner <willybrauner@gmail.com>
2023-12-03 22:04:21 +0100

    part-2

--- 2023/day-03/index.ts
+++ 2023/day-03/index.ts
@@ -28,7 +28,6 @@ type Input = {
     ...
   }
 }
-
  */
 const format = (filename: 'input.test' | 'input'): Input =>
   fs
@@ -68,25 +67,22 @@ const format = (filename: 'input.test' | 'input'): Input =>
     }, {} as Input)
 
 const part1 = ({ nums, symbols }: Input) => {
-  console.log({ nums, symbols })
   let count = 0
   for (let [yx, num] of Object.entries(nums)) {
     const [y, x] = yx.split(',').map((e) => parseInt(e))
 
-    let adj = [-1, 0, 1].reduce((a, b) => {
-      return [
+    let adj = [-1, 0, 1].reduce(
+      (a, b) => [
         ...a,
         ...new Array(num.length + 2)
           .fill(null)
           .map((e, i) => `${Math.max(0, y + b)},${Math.max(0, x + i - 1)}`),
-      ]
-    }, [])
+      ],
+      [],
+    )
     adj = [...new Set(adj)]
-    for (let [symbolYX, symbol] of Object.entries(symbols)) {
-      if (adj.includes(symbolYX)) {
-        log(`symbolYX ${symbolYX} ${symbol} match with ${num}`, adj)
-        count += parseInt(num)
-      }
+    for (let [symbolYX] of Object.entries(symbols)) {
+      if (adj.includes(symbolYX)) count += parseInt(num)
     }
   }
   return count
@@ -94,7 +90,45 @@ const part1 = ({ nums, symbols }: Input) => {
 
 log(part1(format('input')))
 
-const part2 = (input: Input) => {
-  return input
+const part2 = ({ nums, symbols }: Input) => {
+  const keep: Record<string, number[]> = {}
+
+  for (let [symbolYX, symbol] of Object.entries(symbols)) {
+    const [y, x] = symbolYX.split(',').map((e) => parseInt(e))
+    let symbolAdj = [-1, 0, 1].reduce(
+      (a, b) => [
+        ...a,
+        ...new Array(3)
+          .fill(null)
+          .map((e, i) => `${Math.max(0, y + b)},${Math.max(0, x + i - 1)}`),
+      ],
+      [],
+    )
+
+    for (let [yx, num] of Object.entries(nums)) {
+      const [y, x] = yx.split(',').map((e) => parseInt(e))
+      const numCoords = new Array(num.length)
+        .fill(null)
+        .map((e, i) => `${y},${x + i}`)
+
+      const arrayEntryMatch = (arr1, arr2) => {
+        for (let i = 0; i < arr1.length; i++)
+          if (arr2.includes(arr1[i])) return true
+      }
+
+      if (arrayEntryMatch(numCoords, symbolAdj) && symbol === '*') {
+        if (!keep[symbolYX]) keep[symbolYX] = []
+        keep[symbolYX].push(parseInt(num))
+      }
+    }
+  }
+
+  // multiply values if two numbers are associated to * coords
+  // ex: { '1,3': [ 467, 35 ], '4,3': [ 617 ], ... }
+  return Object.values(keep).reduce(
+    (acc, curr) =>
+      curr.length === 2 ? acc + curr.reduce((a, b) => a * b) : acc,
+    0,
+  )
 }
-part2(format('input.test'))
+log(part2(format('input.test')))

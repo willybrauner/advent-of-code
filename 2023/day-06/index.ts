ae7171eb887a448cffb0233c70babe3671776d1e
Willy Brauner <willybrauner@gmail.com>
2023-12-07 07:42:24 +0100

    2023 day 6 part 2

--- 2023/day-06/index.ts
+++ 2023/day-06/index.ts
@@ -1,6 +1,6 @@
 /**
- * XXXX - Day X
- * https://adventofcode.com/2022/day/X
+ * 2023 - Day 6
+ * https://adventofcode.com/2023/day/6
  */
 
 import fs from 'fs'
@@ -8,17 +8,40 @@ import path from 'path'
 const { log, clear } = console
 clear()
 
-type Input = any
+type Input = string[][]
 const format = (filename: 'input.test' | 'input'): Input =>
-  fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .map((e) => e.split(' ').filter((e) => /\d/.test(e)))
 
-
-const part1 = (input: Input) => {
-  return input
+const run = (time, distance): number => {
+  const won = []
+  for (let i = 1; i < time; i++) {
+    const millimeters = (time - i) * i
+    if (millimeters > distance) won.push(millimeters)
+  }
+  return won.length
 }
-log(part1(format('input.test')))
+
+const part1 = (input: Input): number =>
+  input
+    .map((e) => e.map((e) => parseInt(e)))
+    .reduce((a: number[][], b) => {
+      b.forEach((e, i) => {
+        a[i] = a[i] || []
+        a[i].push(e)
+      })
+      return a
+    }, [])
+    .reduce((a, [time, distance]) => a * run(time, distance), 1)
+
+log(part1(format('input')))
 
 const part2 = (input: Input) => {
-  return input
+  const [time, distance] = input.map((e) => parseInt(e.join('')))
+  return run(time, distance)
 }
-part2(format('input.test'))
+
+log(part2(format('input')))

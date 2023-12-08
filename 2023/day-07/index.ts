6990d155a8f85d326830bcee1252c18f85767943
Willy Brauner <willybrauner@gmail.com>
2023-12-08 01:00:08 +0100

    2023 day 7 part 1 wip

--- 2023/day-07/index.ts
+++ 2023/day-07/index.ts
@@ -8,74 +8,62 @@ import path from 'path'
 const { log, clear } = console
 clear()
 
-type Input = [string, number][]
-const format = (filename: 'input.test' | 'input'): Input =>
-  fs
+type Hands = [string, number, number]
+type Input = Hands[]
+
+const handType = (hand: string) => {
+  const h = hand.split('')
+  const checkHand = (common, diff): boolean =>
+    h.some(
+      (el) =>
+        h.filter((e) => e === el).length === common &&
+        new Set(h.filter((e) => e !== el)).size === diff,
+    )
+  return [
+    [5, 0, 6],
+    [4, 1, 5],
+    [3, 1, 4],
+    [3, 2, 3],
+    [2, 2, 2],
+    [2, 3, 1],
+    [1, 4, 0],
+  ].find((e) => checkHand(e[0], e[1]))[2]
+}
+
+const order = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
+const sortHands = (hands: Input) =>
+  hands.sort((a, b) => {
+    if (a[2] > b[2]) return -1
+    if (a[2] < b[2]) return 1
+    if (a[2] === b[2]) {
+      if (a[0][0] === b[0][0]) {
+        return sortHands([
+          [a[0].slice(1), a[1], a[2]],
+          [b[0].slice(1), b[1], b[2]],
+        ])
+      }
+      const aIndex = order.reverse().findIndex((e) => e === a[0][0])
+      const bIndex = order.reverse().findIndex((e) => e === b[0][0])
+      if (aIndex > bIndex) return -1
+      if (aIndex < bIndex) return 1
+    }
+  })
+
+const part1 = (filename) => {
+  const input = fs
     .readFileSync(path.resolve(__dirname, filename), 'utf8')
     .split('\n')
     .filter(Boolean)
     .map((e) => e.split(' '))
-    .map((e) => [e[0], parseInt(e[1])])
-
-const handType = (hand: string) => {
-  const h = hand.split('')
-  // 5 same
-  const isFive = new Set(h).size === 1
-  // 4 + 1
-  const isFour = h.some(
-    (el) =>
-      h.filter((e) => e === el).length === 4 &&
-      h.filter((e) => e !== el).length === 1,
-  )
-  // 3 + 2 same
-  const isThreeFH = h.some(
-    (el) =>
-      h.filter((e) => e === el).length === 3 &&
-      new Set(h.filter((e) => e !== el)).size === 1,
-  )
-  // 3 + 2 diff
-  const isThree = h.some(
-    (el) =>
-      h.filter((e) => e === el).length === 3 &&
-      new Set(h.filter((e) => e !== el)).size === 2,
-  )
-  // 2 + 2 + 1
-  const isTwoPair = h.some(
-    (el) =>
-      h.filter((e) => e === el).length === 2 &&
-      new Set(h.filter((e) => e !== el)).size === 2,
-  )
-  // 2 + 1 + 1 + 1
-  const isOnePair = h.some(
-    (el) =>
-      h.filter((e) => e === el).length === 2 &&
-      new Set(h.filter((e) => e !== el)).size === 3,
-  )
-  // 1 + 1 + 1 + 1 + 1
-  const isHighCard = new Set(h).size === 5
-
-  if (isFive) return 'Five of a kind'
-  if (isFour) return 'Four of a kind'
-  if (isThreeFH) return 'Full house'
-  if (isThree) return 'Three of a kind'
-  if (isTwoPair) return 'Two pair'
-  if (isOnePair) return 'One pair'
-  if (isHighCard) return 'High card'
+    .map((e) => [e[0], parseInt(e[1]), handType(e[0])]) as Input
+  const sorted = sortHands(input).reverse()
+  return sorted.reduce((a, b, i) => a + b[1] * (i + 1), 0)
 }
 
-const part1 = (input: Input) => {
-  // put the hands in order of strength
-  for (let [hand] of input) {
-    const type = handType(hand)
-    log('type', type)
-    // ...
-  }
-  // reorder if others get multi hands have the same type
-}
+log(part1('input'))
 
-log(part1(format('input.test')))
-
-const part2 = (input: Input) => {
-  return input
+const part2 = (filename) => {
+  //  return input
 }
-part2(format('input.test'))
+part2('input.test')

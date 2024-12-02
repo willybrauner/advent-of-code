bd2148a8bb38df965546df747c524081eaf3e808
Willy Brauner <willybrauner@gmail.com>
2024-12-02 22:31:53 +0100

    Remove old tests

--- 2022/day-05/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { part1, part2 } from './index'
-
-it('resolve part 1', () => {
-  expect(part1()).toBe('CMZ')
-})
-
-it('resolve part 2', () => {
-  expect(part2()).toBe('MCD')
-})
--- 2022/day-07/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { part1, part2 } from './index'
-
-it('resolve part 1', () => {
-  //console.log(part1())
-})
-
-it('resolve part 2', () => {
-  // console.log(part2())
-})

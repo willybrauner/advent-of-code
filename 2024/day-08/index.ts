8c6f4909bc43cb89ccf7812b32eb3ec36849899c
Willy Brauner <willybrauner@gmail.com>
2024-12-10 11:12:35 +0100

    2024 - Day 08 part 2

--- 2024/day-08/index.ts
+++ 2024/day-08/index.ts
@@ -44,13 +44,37 @@ const part1 = (input: Input) => {
         if (!(y > input.length - 1 || y < 0 || x > input[0].length - 1 || x < 0)) 
           antinodes.add(`${y},${x}`)        
       }
-    
   })
   return antinodes.size
 }
 log(part1(useInput('input')))
 
+// prettier-ignore
 const part2 = (input: Input) => {
-  return input
+  const antennas = getAntennas(input)
+  const antinodes = new Set()
+  antennas.forEach((list) => {
+    // On part2, ensure that all antenna positions aligned with others are added
+    if (list.length > 1) list.forEach(([y, x]) => antinodes.add(`${y},${x}`))
+      
+    for (let i = 0; i < list.length; i++)
+      for (let j = 0; j < list.length; j++) {
+        if (i === j) continue
+        const [y1, x1] = list[i]
+        const [y2, x2] = list[j]       
+        
+        // On part2, we have to check the antinodes position recurcively
+        const antinodePosition = ([y1, x1]: Coords, [y2, x2]: Coords) => {
+          const y = y2 + (y2 - y1)
+          const x = x2 + (x2 - x1)
+          if (!(y > input.length - 1 || y < 0 || x > input[0].length - 1 || x < 0)) {
+            antinodes.add(`${y},${x}`)
+            antinodePosition([y2, x2], [y, x])
+          }
+        }
+        antinodePosition([y1, x1], [y2, x2])
+      }
+  })
+  return antinodes.size
 }
-part2(useInput('input.test'))
+log(part2(useInput('input')))

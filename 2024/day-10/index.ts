36d2ac9b67f1270476dbb342d21b37aca7644e20
Willy Brauner <willybrauner@gmail.com>
2024-12-10 13:20:39 +0100

    2024 - Day 10 part 1

---
+++ 2024/day-10/index.ts
@@ -0,0 +1,69 @@
+/**
+ * 2024 - Day 10
+ * https://adventofcode.com/2024/day/10
+ */
+import fs from 'fs'
+import path from 'path'
+const { log, clear } = console
+clear()
+
+type Input = (string | number)[][]
+type Coords = [number, number]
+const useInput = (filename: 'input.test' | 'input'): Input =>
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .map((e) => e.split('').map((e) => (e !== '.' ? parseInt(e) : e)))
+
+const getTrailheads = (input: Input): Coords[] => {
+  const trailheads: Coords[] = []
+  for (let y = 0; y < input.length; y++)
+    for (let x = 0; x < input.length; x++)
+      if (input[y][x] === 0) trailheads.push([y, x])
+  return trailheads
+}
+
+// prettier-ignore
+const score = (input: Input, [sy, sx]: Coords): number => {
+  const dirs = [[1, 0],[0, 1],[-1, 0],[0, -1]]
+  const queue: Coords[] = [[sy, sx]]
+  const visited = new Set<string>()
+  visited.add(`${sy},${sx}`)
+  let count = 0
+
+  while (queue.length) {
+    const [y, x] = queue.pop()
+    visited.add(`${y},${x}`)
+
+    for (const [dy, dx] of dirs) {
+      const [ny, nx] = [y + dy, x + dx]
+      const curr = input?.[y]?.[x]
+      const next = input?.[ny]?.[nx]
+
+      if (
+        next === (curr as number) + 1 && 
+        !visited.has(`${ny},${nx}`)
+      ) {
+        queue.push([ny, nx])
+        visited.add(`${ny},${nx}`)
+        if (next === 9) count++
+      }
+    }
+  }
+  
+  return count
+}
+
+const part1 = (input: Input) =>
+  getTrailheads(input).reduce(
+    (a: number, coords: Coords) => a + score(input, coords),
+    0,
+  )
+
+log(part1(useInput('input')))
+
+const part2 = (input: Input) => {
+  return input
+}
+part2(useInput('input.test'))

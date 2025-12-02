de109b5276c25082e6409c1b24e2a1923a8e69e7
Willy Brauner <willybrauner@gmail.com>
2025-12-02 12:10:09 +0100

    2025 - Day 2 part 1

---
+++ 2025/day-02/index.ts
@@ -0,0 +1,49 @@
+/**
+ * 2025 - Day 2
+ * https://adventofcode.com/2025/day/2
+ */
+import fs from 'fs'
+import path from 'path'
+const { log, clear } = console
+clear()
+
+type Input = number[][]
+const useInput = (filename: 'input.test' | 'input'): Input =>
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split(',')
+    .map((e) => e.split('-').map((e) => parseFloat(e)))
+
+function _chunkArr<T>(arr: T[], size: number): T[][] {
+  let res = []
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, size + i))
+  }
+  return res
+}
+
+const part1 = (input: Input) => {
+  let invalids = 0
+  for (let i = 0; i < input.length; i++) {
+    const [start, end] = input[i]
+    for (let s = start; s <= end; s++) {
+      const numToStrings = s.toString().split('')
+      
+      // check if even
+      if (numToStrings.length % 2 !== 0) continue
+      const [part1, part2] = _chunkArr(numToStrings, numToStrings.length / 2)
+      const toNum = (e: string[]) => parseInt(e.join(''))
+
+      // register invalid if 2 parts match
+      if (toNum(part1) === toNum(part2)) invalids += toNum(numToStrings)
+    }
+  }
+
+  return invalids
+}
+log(part1(useInput('input')))
+
+const part2 = (input: Input) => {
+  return input
+}
+part2(useInput('input.test'))

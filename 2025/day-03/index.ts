dbf155e9c469b98780573843f9e3b9c131f4e079
Willy Brauner <willybrauner@gmail.com>
2025-12-03 10:48:31 +0100

    2025 - Day 3 part 1

---
+++ 2025/day-03/index.ts
@@ -0,0 +1,40 @@
+/**
+ * XXXX - Day X
+ * https://adventofcode.com/XXXX/day/X
+ */
+import fs from 'fs'
+import path from 'path'
+const { log, clear } = console
+clear()
+
+type Input = number[][]
+const useInput = (filename: 'input.test' | 'input'): Input =>
+  fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .split('\n')
+    .filter(Boolean)
+    .map((e) => e.split('').map((e) => parseInt(e)))
+
+const part1 = (input: Input) => {
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const batteries = input[i]
+    let largest = 0
+    for (let y = 0; y < batteries.length; y++) {
+      for (let n = y; n < batteries.length + 1; n++) {
+        if (y === n) continue
+        const curr = parseInt(`${batteries[y]}${batteries[n]}`)
+        if (curr > largest) largest = curr
+      }
+    }
+    count += largest
+  }
+  return count
+}
+
+log(part1(useInput('input')))
+
+const part2 = (input: Input) => {
+  return input
+}
+part2(useInput('input.test'))

b258af02ece6267ba541968dcdaa03fe081a6cd3
Willy Brauner <willybrauner@gmail.com>
2025-12-04 22:03:55 +0100

    2025 - Day 4 part 2, shorter

--- 2025/day-04/index.ts
+++ 2025/day-04/index.ts
@@ -27,46 +27,38 @@ const getNeighbors = (input: Input, y: number, x: number) => {
   ].filter(Boolean)
 }
 
+// prettier-ignore
 const part1 = (input: Input) => {
   let accessed = 0
-  for (let y = 0; y < input.length; y++) {
+  for (let y = 0; y < input.length; y++) 
     for (let x = 0; x < input[y].length; x++) {
-      const cell = input[y][x]
-      if (cell === '.') continue
-      // prettier-ignore
+      if (input[y][x] === '.') continue
       accessed += getNeighbors(input, y, x)
         .reduce((a, b) => (b == '@' ? (a += 1) : a) ,0) < 4 ? 1 : 0
     }
-  }
   return accessed
 }
-console.log(part1(useInput('input')))
 
+part1(useInput('input'))
+
+// prettier-ignore
 const part2 = (input: Input) => {
   let accessed = 0
   let lastAccessed = null
-
   while (lastAccessed !== accessed) {
-    // register last accessed count
     lastAccessed = accessed
-
     const accessedCells: [number, number][] = []
-    for (let y = 0; y < input.length; y++) {
+    for (let y = 0; y < input.length; y++) 
       for (let x = 0; x < input[y].length; x++) {
-        const cell = input[y][x]
-        if (cell === '.' || cell === 'x') continue
-        // prettier-ignore
+        if (input[y][x] === '.' || input[y][x]=== 'x') continue
         const isAccessed =
           getNeighbors(input, y, x)
             .reduce((a, b) => (b == '@' ? (a += 1) : a), 0) < 4
-
         if (isAccessed) {
           accessed += 1
           accessedCells.push([y, x])
         }
-      }
-    }
-    // Mark accessed cells
+      }    
     accessedCells.forEach(([y, x]) => (input[y][x] = 'x'))
   }
   return accessed

a562a30afb07f30eb602d62dbac0a5e8092f9aa5
Willy Brauner <willybrauner@gmail.com>
2025-12-04 21:57:04 +0100

    2025 - Day 4 part 2

--- 2025/day-04/index.ts
+++ 2025/day-04/index.ts
@@ -39,9 +39,44 @@ const part1 = (input: Input) => {
   }
   return accessed
 }
-log(part1(useInput('input')))
+console.log(part1(useInput('input.test')))
 
 const part2 = (input: Input) => {
-  return input
+  let accessed = 0
+  let lastAccessed = null
+
+  while (lastAccessed !== accessed) {
+    lastAccessed = accessed
+
+    const accessedCells: [number, number][] = []
+    for (let y = 0; y < input.length; y++) {
+      for (let x = 0; x < input[y].length; x++) {
+        const cell = input[y][x]
+        if (cell === '.' || cell === 'x') continue
+        const isAccessed =
+          [
+            input[y - 1]?.[x],
+            input[y - 1]?.[x + 1],
+            input[y]?.[x + 1],
+            input[y + 1]?.[x + 1],
+            input[y + 1]?.[x],
+            input[y + 1]?.[x - 1],
+            input[y]?.[x - 1],
+            input[y - 1]?.[x - 1],
+          ]
+            .filter(Boolean)
+            .reduce((a, b) => (b == '@' ? (a += 1) : a), 0) < 4
+
+        if (isAccessed) {
+          accessed += 1
+          accessedCells.push([y, x])
+        }
+      }
+    }
+    // Mark accessed cells
+    accessedCells.forEach(([y, x]) => (input[y][x] = 'x'))
+  }
+  return accessed
 }
-part2(useInput('input.test'))
+
+log(part2(useInput('input')))

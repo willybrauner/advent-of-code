89e7e6decededb9f05dda4e30abdec04d5f5a62e
Willy Brauner <willybrauner@gmail.com>
2025-12-04 22:01:24 +0100

    2025 - Day 4 part 2, refacto

--- 2025/day-04/index.ts
+++ 2025/day-04/index.ts
@@ -14,38 +14,40 @@ const useInput = (filename: 'input.test' | 'input'): Input =>
     .split('\n')
     .map((e) => e.split(''))
 
+const getNeighbors = (input: Input, y: number, x: number) => {
+  return [
+    input[y - 1]?.[x],
+    input[y - 1]?.[x + 1],
+    input[y]?.[x + 1],
+    input[y + 1]?.[x + 1],
+    input[y + 1]?.[x],
+    input[y + 1]?.[x - 1],
+    input[y]?.[x - 1],
+    input[y - 1]?.[x - 1],
+  ].filter(Boolean)
+}
+
 const part1 = (input: Input) => {
   let accessed = 0
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
       const cell = input[y][x]
       if (cell === '.') continue
-      accessed +=
-        [
-          input[y - 1]?.[x],
-          input[y - 1]?.[x + 1],
-          input[y]?.[x + 1],
-          input[y + 1]?.[x + 1],
-          input[y + 1]?.[x],
-          input[y + 1]?.[x - 1],
-          input[y]?.[x - 1],
-          input[y - 1]?.[x - 1],
-        ]
-          .filter(Boolean)
-          .reduce((a, b) => (b == '@' ? (a += 1) : a), 0) < 4
-          ? 1
-          : 0
+      // prettier-ignore
+      accessed += getNeighbors(input, y, x)
+        .reduce((a, b) => (b == '@' ? (a += 1) : a) ,0) < 4 ? 1 : 0
     }
   }
   return accessed
 }
-console.log(part1(useInput('input.test')))
+console.log(part1(useInput('input')))
 
 const part2 = (input: Input) => {
   let accessed = 0
   let lastAccessed = null
 
   while (lastAccessed !== accessed) {
+    // register last accessed count
     lastAccessed = accessed
 
     const accessedCells: [number, number][] = []
@@ -53,18 +55,9 @@ const part2 = (input: Input) => {
       for (let x = 0; x < input[y].length; x++) {
         const cell = input[y][x]
         if (cell === '.' || cell === 'x') continue
+        // prettier-ignore
         const isAccessed =
-          [
-            input[y - 1]?.[x],
-            input[y - 1]?.[x + 1],
-            input[y]?.[x + 1],
-            input[y + 1]?.[x + 1],
-            input[y + 1]?.[x],
-            input[y + 1]?.[x - 1],
-            input[y]?.[x - 1],
-            input[y - 1]?.[x - 1],
-          ]
-            .filter(Boolean)
+          getNeighbors(input, y, x)
             .reduce((a, b) => (b == '@' ? (a += 1) : a), 0) < 4
 
         if (isAccessed) {

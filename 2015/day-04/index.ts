58b3479b218b526e61178e6dbf4ca79a084dcd5d
Willy Brauner <willybrauner@gmail.com>
2026-07-23 23:14:33 +0200

    2015 - Day 4 part 1 & 2

---
+++ 2015/day-04/index.ts
@@ -0,0 +1,34 @@
+/**
+ * 2015 - Day 4
+ * https://adventofcode.com/2015/day/4
+ */
+import fs from 'fs'
+import path from 'path'
+import { createHash, Hash } from 'node:crypto'
+const { log, clear } = console
+clear()
+
+type Input = string
+const useInput = (filename: 'input.test' | 'input'): Input =>
+  fs.readFileSync(path.resolve(__dirname, filename), 'utf8').trim()
+
+const part1 = (input: Input) => {
+  let count = 1
+  while (true) {
+    const hash = createHash('md5').update(input + count).digest('hex')
+    if (hash.startsWith('00000')) break
+    count++
+  }
+  return count
+}
+
+const part2 = (input: Input) => {
+  let count = 1
+  while (true) {
+    const hash = createHash('md5').update(input + count).digest('hex')
+    if (hash.startsWith('000000')) break
+    count++
+  }
+  return count
+}
+log(part2(useInput('input')))
